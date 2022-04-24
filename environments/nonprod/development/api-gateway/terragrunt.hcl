9f01e25cbb6a378af7bf53b3633838b10e0b14b0
tniu <tniu@infomedia.com.au>
2022-04-25 09:22:26 +1000

    add api gateway and step function

---
+++ environments/nonprod/development/api-gateway/terragrunt.hcl
@@ -0,0 +1,21 @@
+include "root" {
+  path = find_in_parent_folders()
+}
+
+terraform {
+  source = "${get_path_to_repo_root()}//modules/api-gateway"
+}
+
+//dependency "s3" {
+//  config_path = "../../storage/s3"
+//}
+
+locals {
+  env_vars = read_terragrunt_config(find_in_parent_folders())
+}
+
+inputs = {
+  tags = {
+    "Managed By" = "Terragrunt"
+  }
+}
---
+++ modules/step-functions/step-function.tf
@@ -0,0 +1,28 @@
+module "step_function" {
+  source = "terraform-aws-modules/step-functions/aws"
+
+  name       = "my-step-function"
+  definition = <<EOF
+{
+  "Comment": "A Hello World example of the Amazon States Language using Pass states",
+  "StartAt": "Hello",
+  "States": {
+    "Hello": {
+      "Type": "Pass",
+      "Result": "Hello",
+      "Next": "World"
+    },
+    "World": {
+      "Type": "Pass",
+      "Result": "World",
+      "End": true
+    }
+  }
+}
+EOF
+  type = "STANDARD"
+
+  tags = {
+    Module = "my"
+  }
+}
